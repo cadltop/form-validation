ff5adee7dd4eef7bbbcd7d55a133dbc6899c3b11
cadltop <158861368+cadltop@users.noreply.github.com>
2024-09-08 11:34:23 -0500

    Select inputs via JavaScript

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
-import 'normalize.css';
-import './index.css';
+import "normalize.css";
+import "./index.css";
+
+const formInputs = {
+  get email() {
+    return document.querySelector("form input#email");
+  },
+  get country() {
+    return document.querySelector("form select#country");
+  },
+  get zipCode() {
+    return document.querySelector("form input#zip-code");
+  },
+  get password() {
+    return document.querySelector("form input#password");
+  },
+  get confirmPassword() {
+    return document.querySelector("form input#confirm-password");
+  },
+  get submit() {
+    return document.querySelector("form button");
+  },
+};
